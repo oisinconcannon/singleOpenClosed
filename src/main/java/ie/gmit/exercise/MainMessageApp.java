fd3a51956f2136152cae895cec0f1da96d6a1b16
Oisin <G00347603@gmit.ie>
2021-05-02 23:47:05 +0100

    Implementing Dependancy Inversion Rule

--- src/main/java/ie/gmit/exercise/MainMessageApp.java
+++ src/main/java/ie/gmit/exercise/MainMessageApp.java
@@ -1,14 +1,19 @@
 package ie.gmit.exercise;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainMessageApp {
 
 	public static void main(String[] args) throws IOException {
 
+
+		Formatter formatter = new JSONFormatter();//creates formatter
+		PrintWriter writer = new PrintWriter(new FileWriter("test_msg.txt"));
+
 		Message msg = new Message("This is a message");
 		MessagePrinter printer = new MessagePrinter();
-		printer.writeMessage(msg, "test_msg.txt");
+		printer.writeMessage(msg,formatter,writer);
 	}
-
 }
